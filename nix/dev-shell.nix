08db98efd4c881a3c23895c2e452df02b63f6966
oddlama <oddlama@oddlama.org>
2022-12-09 15:46:22 +0100

    feat: add dev-shell

---
+++ nix/dev-shell.nix
@@ -0,0 +1,37 @@
+{self, ...}: system:
+with self.pkgs.${system};
+  mkShell {
+    name = "nix-config";
+
+    nativeBuildInputs = [
+      # Nix
+      cachix
+      deploy-rs.deploy-rs
+      nix-build-uncached
+      nix-linter
+      alejandra
+      ragenix
+      rnix-lsp
+      statix
+
+      # Lua
+      stylua
+      (luajit.withPackages (p: with p; [luacheck]))
+      sumneko-lua-language-server
+
+      # GitHub Actions
+      act
+      actionlint
+      python3Packages.pyflakes
+      shellcheck
+
+      # Misc
+      jq
+      pre-commit
+      rage
+    ];
+
+    shellHook = ''
+      ${self.checks.${system}.pre-commit-check.shellHook}
+    '';
+  }

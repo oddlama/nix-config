6c5d117c1e9bd330915a4a04a8e62576f191bbb8
oddlama <oddlama@oddlama.org>
2023-03-25 16:00:51 +0100

    refactor(zackbiene): use new dnsmasq.settings interface

--- hosts/zackbiene/dnsmasq.nix
+++ hosts/zackbiene/dnsmasq.nix
@@ -2,18 +2,20 @@
   services.dnsmasq = {
     enable = true;
     resolveLocalQueries = false;
-    extraConfig = ''
-      interface=wlan1
+    settings = {
+      interface = "wlan1";
+      dhcp-authoritative = true;
+      dhcp-range = [
+        "10.0.90.10,10.0.90.240,24h"
+        "fd90::10,fd90::ff0,24h"
+      ];
 
-      dhcp-authoritative
-      dhcp-range=10.0.90.10,10.0.90.240,24h
-      dhcp-range=fd90::10,fd90::ff0,24h
-
-      enable-ra
+      # Enable ipv6 router advertisements
+      enable-ra = true;
+      # Don't use anything from /etc/resolv.conf
+      no-resolv = true;
       # Never forward addresses in the non-routed address spaces.
-      bogus-priv
-
-      no-resolv
-    '';
+      bogus-priv = true;
+    };
   };
 }

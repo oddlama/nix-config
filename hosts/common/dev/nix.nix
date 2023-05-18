e5f3ffd288a472861221265e0649d091f6322a52
oddlama <oddlama@oddlama.org>
2023-05-18 11:48:09 +0200

    fix: reference extra-builtins.nix with access to relative files

--- hosts/common/dev/nix.nix
+++ hosts/common/dev/nix.nix
@@ -1,6 +1,8 @@
 {pkgs, ...}: {
+  # Make sure not to reference the extra-builtins file directly but
+  # at least via its parent folder so it can access relative files.
   nix.extraOptions = ''
     plugin-files = ${pkgs.nix-plugins}/lib/nix/plugins
-    extra-builtins-file = ${../../../nix/extra-builtins.nix}
+    extra-builtins-file = ${../../../nix}/extra-builtins.nix
   '';
 }

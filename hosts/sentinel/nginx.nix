ef03fd059453b6ee1a214d853e1428b7bc0cd2f7
oddlama <oddlama@oddlama.org>
2023-05-31 18:06:53 +0200

    feat: activate acme on sentinel

--- hosts/sentinel/nginx.nix
+++ hosts/sentinel/nginx.nix
@@ -1,13 +1,11 @@
-{config, ...}: let
+{
+  config,
+  nodes,
+  ...
+}: let
   inherit (config.repo.secrets.local) acme personalDomain;
 in {
   networking.domain = personalDomain;
-
-  rekey.secrets."dhparams.pem" = {
-    file = ./secrets/dhparams.pem.age;
-    mode = "440";
-    group = "nginx";
-  };
 
   rekey.secrets.acme-credentials = {
     file = ./secrets/acme-credentials.age;
@@ -15,37 +13,45 @@ in {
     group = "acme";
   };
 
-  #security.acme = {
-  #  acceptTerms = true;
-  #  defaults = {
-  #    inherit (acme) email;
-  #    credentialsFile = config.rekey.secrets.acme-credentials.path;
-  #    dnsProvider = "cloudflare";
-  #    dnsPropagationCheck = true;
-  #    reloadServices = ["nginx"];
-  #  };
-  #};
-  #extra.acme.wildcardDomains = acme.domains;
-  #users.groups.acme.members = ["nginx"];
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      inherit (acme) email;
+      credentialsFile = config.rekey.secrets.acme-credentials.path;
+      dnsProvider = "cloudflare";
+      dnsPropagationCheck = true;
+      reloadServices = ["nginx"];
+    };
+  };
+  extra.acme.wildcardDomains = acme.domains;
+  users.groups.acme.members = ["nginx"];
 
-  #services.nginx = {
-  #  enable = true;
-  #  upstreams."kanidm" = {
-  #    servers."${config.extra.wireguard."${parentNodeName}-local-vms".ipv4}:8300" = {};
-  #    extraConfig = ''
-  #      zone kanidm 64k;
-  #      keepalive 2;
-  #    '';
-  #  };
-  #  virtualHosts.${authDomain} = {
-  #    forceSSL = true;
-  #    useACMEHost = config.lib.extra.matchingWildcardCert authDomain;
-  #    locations."/".proxyPass = "https://kanidm";
-  #    # Allow using self-signed certs to satisfy kanidm's requirement
-  #    # for TLS connections. (This is over wireguard anyway)
-  #    extraConfig = ''
-  #      proxy_ssl_verify off;
-  #    '';
-  #  };
-  #};
+  rekey.secrets."dhparams.pem" = {
+    file = ./secrets/dhparams.pem.age;
+    mode = "440";
+    group = "nginx";
+  };
+
+  services.nginx = let
+    authDomain = nodes.ward-nginx.config.services.kanidm.serverSettings.domain;
+  in {
+    enable = true;
+    upstreams."kanidm" = {
+      servers."${nodes.ward-nginx.config.extra.wireguard.proxy-sentinel.ipv4}:8300" = {};
+      extraConfig = ''
+        zone kanidm 64k;
+        keepalive 2;
+      '';
+    };
+    virtualHosts.${authDomain} = {
+      forceSSL = true;
+      useACMEHost = config.lib.extra.matchingWildcardCert authDomain;
+      locations."/".proxyPass = "https://kanidm";
+      # Allow using self-signed certs to satisfy kanidm's requirement
+      # for TLS connections. (This is over wireguard anyway)
+      extraConfig = ''
+        proxy_ssl_verify off;
+      '';
+    };
+  };
 }

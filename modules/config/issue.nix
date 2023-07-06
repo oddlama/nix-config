f2a97b16c6e6ecc822405928530fe85cafbd10ed
oddlama <oddlama@oddlama.org>
2023-07-06 12:18:36 +0200

    chore: remove ip address from etc issue again due to frequent microvm redraws

--- modules/config/issue.nix
+++ modules/config/issue.nix
@@ -1,12 +1,10 @@
-let
+{lib, ...} @ args: {
   # IP addresses: ${"${interface} \e{halfbright}\4{${interface}}\e{reset} \e{halfbright}\6{${interface}}\e{reset}"}
-  issue_text = ''
-    \d  \t
-    \e{halfbright}\4\e{reset} \e{halfbright}\6\e{reset}
-    This is \e{cyan}\n\e{reset} [\e{lightblue}\l\e{reset}] (\s \m \r)
-
-  '';
-in {
-  environment.etc."issue".text = issue_text;
-  environment.etc."issue.logo".text = issue_text;
+  environment.etc.issue.text = lib.concatStringsSep "\n" ([
+      ''\d  \t''
+      ''This is \e{cyan}\n\e{reset} [\e{lightblue}\l\e{reset}] (\s \m \r)''
+    ]
+    # Disabled for microvms because of frequent redraws (-> pushed to syslog on the host)
+    ++ lib.optional (!(args ? parentNodeName)) ''\e{halfbright}\4\e{reset} \e{halfbright}\6\e{reset}''
+    ++ [""]);
 }

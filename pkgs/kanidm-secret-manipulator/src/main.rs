6063da387b41df96a6231b8cfe05b0bf42c25370
oddlama <oddlama@oddlama.org>
2023-08-23 00:49:13 +0200

    feat(kanidm-secret-manipulator): allow rewriting account credentials

--- pkgs/kanidm-secret-manipulator/src/main.rs
+++ pkgs/kanidm-secret-manipulator/src/main.rs
@@ -1,13 +1,34 @@
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
+use argon2::{Algorithm, Argon2};
+use base64urlsafedata::Base64UrlSafeData;
+use rand::Rng;
 use rusqlite::{params, Connection};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs::File;
 
+#[derive(Debug, Deserialize, Serialize)]
+struct Argon2IDHash {
+    m: u32,
+    t: u32,
+    p: u32,
+    v: u32,
+    s: Base64UrlSafeData,
+    k: Base64UrlSafeData,
+}
+
 #[derive(Debug, Deserialize)]
 struct SecretMappings {
+    account_credentials: HashMap<String, String>,
     oauth2_basic_secrets: HashMap<String, String>,
 }
+
+const ACCOUNT_NAME_PATH: &str = "/ent/V2/attrs/name/N8/0";
+const ACCOUNT_PRIMARY_CREDENTIAL_PATH: &str =
+    "/ent/V2/attrs/primary_credential/CR/0/d/password/ARGON2ID";
+
+const OAUTH2_BASIC_SECRET_PATH: &str = "/ent/V2/attrs/oauth2_rs_basic_secret/RU";
+const OAUTH2_NAME_PATH: &str = "/ent/V2/attrs/oauth2_rs_name/N8/0";
 
 fn main() -> Result<()> {
     // Read command-line arguments
@@ -50,39 +71,16 @@ fn main() -> Result<()> {
         let (id, data) = row?;
         let mut json_data: serde_json::Value = serde_json::from_slice(&data)?;
 
-        // Clone paths for updating
-        let oauth2_rs_name_path = json_data
-            .pointer("/ent/V2/attrs/oauth2_rs_name/N8/0")
-            .and_then(|value| value.as_str())
-            .map(|value| value.to_string()); // Clone the value
-        let oauth2_rs_basic_secret_path = "/ent/V2/attrs/oauth2_rs_basic_secret/RU";
+        let mut any_changes = false;
+        any_changes |=
+            rewrite_account_credentials(&mut secret_mappings, &mut json_data).unwrap_or(false);
+        any_changes |= rewrite_oauth2_secret(&mut secret_mappings, &mut json_data).unwrap_or(false);
 
-        if let Some(oauth2_rs_name) = oauth2_rs_name_path {
-            if let Some(secret_path) = secret_mappings.oauth2_basic_secrets.remove(&oauth2_rs_name)
-            {
-                match std::fs::read_to_string(&secret_path) {
-                    Ok(secret) => {
-                        let secret = secret.trim();
-
-                        // Update the cloned JSON data
-                        if let Some(value) = json_data.pointer_mut(oauth2_rs_basic_secret_path) {
-                            *value = serde_json::Value::String(secret.to_string());
-                        }
-
-                        // Update the row in the database
-                        update_stmt
-                            .execute(params![serde_json::to_vec(&json_data)?, id])
-                            .context(
-                                "Failed to update oauth2 service '{oauth2_rs_name}' in database",
-                            )?;
-
-                        println!("oauth2: Updated {oauth2_rs_name}");
-                    }
-                    Err(e) => eprintln!(
-                        "oauth2: Could not update {oauth2_rs_name} with {secret_path}: {e}"
-                    ),
-                }
-            }
+        // Update the row in the database if necessary
+        if any_changes {
+            update_stmt
+                .execute(params![serde_json::to_vec(&json_data)?, id])
+                .context("Failed to update oauth2 service '{oauth2_rs_name}' in database")?;
         }
     }
 
@@ -100,4 +98,89 @@ fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+fn hash_argon2_id(password: &str, salt: Option<Vec<u8>>) -> Result<Argon2IDHash> {
+    let salt_len: usize = 16;
+    let key_len: usize = 32;
+    let version: argon2::Version = argon2::Version::V0x13;
+    let m_cost: u32 = 8192;
+    let t_cost: u32 = 2;
+    let p_cost: u32 = 1;
+
+    let params = argon2::Params::new(m_cost, t_cost, p_cost, None).unwrap_or_default();
+    let argon = Argon2::new(Algorithm::Argon2id, version, params);
+
+    let mut rng = rand::thread_rng();
+    let salt: Vec<u8> = salt.unwrap_or_else(|| (0..salt_len).map(|_| rng.gen()).collect());
+    let mut key: Vec<u8> = (0..key_len).map(|_| 0).collect();
+    argon
+        .hash_password_into(password.as_bytes(), salt.as_slice(), key.as_mut_slice())
+        .map_err(|_| anyhow!("Failed to create argon2id hash of password"))?;
+
+    Ok(Argon2IDHash {
+        m: m_cost,
+        t: t_cost,
+        p: p_cost,
+        v: version as u32,
+        s: salt.into(),
+        k: key.into(),
+    })
+}
+
+fn rewrite_account_credentials(
+    secret_mappings: &mut SecretMappings,
+    json_data: &mut serde_json::Value,
+) -> Result<bool> {
+    if let Some(name) = json_data
+        .pointer(ACCOUNT_NAME_PATH)
+        .and_then(|value| value.as_str())
+        .map(|value| value.to_string())
+    {
+        if let Some(secret_path) = secret_mappings.account_credentials.remove(&name) {
+            let secret = std::fs::read_to_string(&secret_path).context(format!(
+                "account: Could not update credential for {name} with {secret_path}",
+            ))?;
+            if let Some(value) = json_data.pointer_mut(ACCOUNT_PRIMARY_CREDENTIAL_PATH) {
+                let secret = secret.trim().to_string();
+                let current_hash: Argon2IDHash = serde_json::from_value(value.clone())
+                    .map_err(|e| anyhow!("Failed to load current argon2id hash: {e}"))?;
+                let test_hash = hash_argon2_id(&secret, Some(current_hash.s.into()))?;
+
+                if current_hash.k != test_hash.k {
+                    let new_hash = hash_argon2_id(&secret, None)?;
+                    *value = serde_json::to_value(new_hash)?;
+                    println!("account: Updated credential for {name}");
+                    return Ok(true);
+                }
+            }
+        }
+    }
+    Ok(false)
+}
+
+fn rewrite_oauth2_secret(
+    secret_mappings: &mut SecretMappings,
+    json_data: &mut serde_json::Value,
+) -> Result<bool> {
+    if let Some(name) = json_data
+        .pointer(OAUTH2_NAME_PATH)
+        .and_then(|value| value.as_str())
+        .map(|value| value.to_string())
+    {
+        if let Some(secret_path) = secret_mappings.oauth2_basic_secrets.remove(&name) {
+            let secret = std::fs::read_to_string(&secret_path).context(format!(
+                "oauth2: Could not update basic secret for {name} with {secret_path}"
+            ))?;
+            if let Some(value) = json_data.pointer_mut(OAUTH2_BASIC_SECRET_PATH) {
+                let secret = secret.trim().to_string();
+                if *value != secret {
+                    *value = serde_json::Value::String(secret);
+                    println!("oauth2: Updated basic secret for {name}");
+                    return Ok(true);
+                }
+            }
+        }
+    }
+    Ok(false)
 }

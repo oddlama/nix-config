406fedf6379b87b4be74a54d8f962586e0b06c11
oddlama <oddlama@oddlama.org>
2023-09-04 16:16:48 +0200

    fix: update pipewire sound to newer module syntax

--- modules/optional/sound.nix
+++ modules/optional/sound.nix
@@ -12,24 +12,23 @@
     alsa.enable = true;
     jack.enable = true;
     pulse.enable = true;
-    media-session.enable = false;
     wireplumber.enable = true;
-    config = {
-      pipewire."context.properties"."default.clock.allowed-rates" = [
-        44100
-        48000
-        88200
-        96000
-        176400
-        192000
-        358000
-        384000
-        716000
-        768000
-      ];
-      pipewire-pulse."stream.properties"."resample.quality" = 15;
-      client."stream.properties"."resample.quality" = 15;
-      client-rt."stream.properties"."resample.quality" = 15;
-    };
+    #config = {
+    #  pipewire."context.properties"."default.clock.allowed-rates" = [
+    #    44100
+    #    48000
+    #    88200
+    #    96000
+    #    176400
+    #    192000
+    #    358000
+    #    384000
+    #    716000
+    #    768000
+    #  ];
+    #  pipewire-pulse."stream.properties"."resample.quality" = 15;
+    #  client."stream.properties"."resample.quality" = 15;
+    #  client-rt."stream.properties"."resample.quality" = 15;
+    #};
   };
 }

d138154633cbb657cd6e75cb6dd24b74f16b4129
oddlama <oddlama@oddlama.org>
2023-09-04 16:17:51 +0200

    fix(installer): use systemd-boot to work around recent grub bug that breaks EFI

--- nix/installer-configuration.nix
+++ nix/installer-configuration.nix
@@ -6,6 +6,8 @@
 
   console.keyMap = "de-latin1-nodeadkeys";
 
+  boot.loader.systemd-boot.enable = true;
+
   users.users.root = {
     password = "nixos";
     openssh.authorizedKeys.keys = ["ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA5Uq+CDy5Pmt3If5M6d8K/Q7HArU6sZ7sgoj3T521Wm"];

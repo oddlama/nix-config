4c377b260eff39519e3bdd2ae6567fa91c87df8f
oddlama <oddlama@oddlama.org>
2023-09-04 16:35:21 +0200

    chore: remove ssh key comment

--- users/myuser/ssh.nix
+++ users/myuser/ssh.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  home.file.".ssh/yubikey.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA5Uq+CDy5Pmt3If5M6d8K/Q7HArU6sZ7sgoj3T521Wm cardno:15 209 174";
+  home.file.".ssh/yubikey.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA5Uq+CDy5Pmt3If5M6d8K/Q7HArU6sZ7sgoj3T521Wm";
   programs.ssh = {
     enable = true;
     matchBlocks = {

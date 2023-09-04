09798a5b2461fe359376b0c753d5cbd43d1ec4fa
oddlama <oddlama@oddlama.org>
2023-09-04 16:18:17 +0200

    chore: remove superfluous dbus enable

--- modules/optional/graphical/wayland.nix
+++ modules/optional/graphical/wayland.nix
@@ -1,10 +1,7 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [wayland];
-  services.dbus.enable = true;
   xdg.portal = {
     enable = true;
     wlr.enable = true;
-    # gtk portal needed to make gtk apps happy
     extraPortals = with pkgs; [xdg-desktop-portal-gtk];
   };
 }

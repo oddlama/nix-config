20758ffa04fbcd856411b59223cabfe5dc32470b
oddlama <oddlama@oddlama.org>
2023-09-18 01:21:51 +0200

    fix: use correct zsh-autosuggestions file path

--- users/modules/config/shell/zsh/default.nix
+++ users/modules/config/shell/zsh/default.nix
@@ -33,6 +33,7 @@
       }
       {
         name = "zsh-autosuggestions";
+        file = "zsh-autosuggestions.zsh";
         src = "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
       }
       {

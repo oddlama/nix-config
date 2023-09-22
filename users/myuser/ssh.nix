5c7f48dfcef97cccaea2b394cfee8b3024304437
oddlama <oddlama@oddlama.org>
2023-09-22 21:43:15 +0200

    feat: add all my ssh config blocks

--- users/myuser/ssh.nix
+++ users/myuser/ssh.nix
@@ -1,26 +1,8 @@
-{
+{config, ...}: {
   home.file.".ssh/yubikey.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA5Uq+CDy5Pmt3If5M6d8K/Q7HArU6sZ7sgoj3T521Wm";
   programs.ssh = {
     enable = true;
-    matchBlocks = {
-      "*" = {
-        identityFile = ["~/.ssh/yubikey.pub"];
-        identitiesOnly = true;
-      };
-      # TODO more from secrets nixosConfiguration.repo.secrets.global
-      meister = {
-        user = "root";
-        hostname = "meister.oddlama.org";
-      };
-      envoy = {
-        user = "root";
-        hostname = "94.130.104.236";
-      };
-      vm-base = {
-        user = "root";
-        proxyJump = "meister";
-        hostname = "172.16.0.01";
-      };
-    };
+    controlMaster = "yes";
+    inherit (config.userSecrets.ssh) matchBlocks;
   };
 }

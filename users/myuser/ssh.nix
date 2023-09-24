54e158d079a65be3233bb151fa4c1edb5272d0e0
oddlama <oddlama@oddlama.org>
2023-09-24 22:56:06 +0200

    fix: use ssh ControlMaster=auto and not yes (which causes errors on 2nd+ connection)

--- users/myuser/ssh.nix
+++ users/myuser/ssh.nix
@@ -2,7 +2,7 @@
   home.file.".ssh/yubikey.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA5Uq+CDy5Pmt3If5M6d8K/Q7HArU6sZ7sgoj3T521Wm";
   programs.ssh = {
     enable = true;
-    controlMaster = "yes";
+    controlMaster = "auto";
     inherit (config.userSecrets.ssh) matchBlocks;
   };
 }

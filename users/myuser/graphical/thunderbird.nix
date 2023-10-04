f37e75da401d1e9a22a8e9019870754255aba2f3
oddlama <oddlama@oddlama.org>
2023-10-05 01:53:39 +0200

    fix: set missing mimetype for thunderbird

--- users/myuser/graphical/thunderbird.nix
+++ users/myuser/graphical/thunderbird.nix
@@ -98,6 +98,7 @@ in {
 
   xdg.mimeApps.defaultApplications = {
     "x-scheme-handler/mailto" = ["thunderbird.desktop"];
+    "x-scheme-handler/mid" = ["thunderbird.desktop"];
     "message/rfc822" = ["thunderbird.desktop"];
   };
 }

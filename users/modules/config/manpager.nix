f6da5b0d6889eaac8dd38bd01d9e86f513309b94
oddlama <oddlama@oddlama.org>
2023-11-16 16:58:09 +0100

    fix: add missing !Man command in new neovim manpager

--- users/modules/config/manpager.nix
+++ users/modules/config/manpager.nix
@@ -3,63 +3,65 @@
   pkgs,
   ...
 }: {
-  home.sessionVariables.MANPAGER = lib.getExe (pkgs.nixvim.makeNixvim {
-    package = pkgs.neovim-clean;
+  home.sessionVariables.MANPAGER = let
+    prg = lib.getExe (pkgs.nixvim.makeNixvim {
+      package = pkgs.neovim-clean;
 
-    options = {
-      buftype = "nowrite";
-      backup = false;
-      modeline = false;
-      shelltemp = false;
-      swapfile = false;
-      undofile = false;
-      writebackup = false;
-      virtualedit = "all";
-      splitkeep = "screen";
-      termguicolors = false;
-    };
+      options = {
+        buftype = "nowrite";
+        backup = false;
+        modeline = false;
+        shelltemp = false;
+        swapfile = false;
+        undofile = false;
+        writebackup = false;
+        virtualedit = "all";
+        splitkeep = "screen";
+        termguicolors = false;
+      };
 
-    extraConfigLua = ''
-      vim.opt.shadafile = vim.fn.stdpath "state" .. "/shada/man.shada";
-    '';
+      extraConfigLua = ''
+        vim.opt.shadafile = vim.fn.stdpath "state" .. "/shada/man.shada";
+      '';
 
-    keymaps = [
-      {
-        action = "<C-]>";
-        key = "<CR>";
-        mode = ["n"];
-        options = {
-          silent = true;
-          desc = "Jump to tag under cursor";
-        };
-      }
-      {
-        action = ":pop<CR>";
-        key = "<BS>";
-        mode = ["n"];
-        options = {
-          silent = true;
-          desc = "Jump to previous tag in stack";
-        };
-      }
-      {
-        action = ":pop<CR>";
-        key = "<C-Left>";
-        mode = ["n"];
-        options = {
-          silent = true;
-          desc = "Jump to previous tag in stack";
-        };
-      }
-      {
-        action = ":tag<CR>";
-        key = "<C-Right>";
-        mode = ["n"];
-        options = {
-          silent = true;
-          desc = "Jump to next tag in stack";
-        };
-      }
-    ];
-  });
+      keymaps = [
+        {
+          action = "<C-]>";
+          key = "<CR>";
+          mode = ["n"];
+          options = {
+            silent = true;
+            desc = "Jump to tag under cursor";
+          };
+        }
+        {
+          action = ":pop<CR>";
+          key = "<BS>";
+          mode = ["n"];
+          options = {
+            silent = true;
+            desc = "Jump to previous tag in stack";
+          };
+        }
+        {
+          action = ":pop<CR>";
+          key = "<C-Left>";
+          mode = ["n"];
+          options = {
+            silent = true;
+            desc = "Jump to previous tag in stack";
+          };
+        }
+        {
+          action = ":tag<CR>";
+          key = "<C-Right>";
+          mode = ["n"];
+          options = {
+            silent = true;
+            desc = "Jump to next tag in stack";
+          };
+        }
+      ];
+    });
+  in "${prg} '+Man!'";
 }

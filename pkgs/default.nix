78987715c2f181ec83fa64e57140f9245b30d7cc
oddlama <oddlama@oddlama.org>
2023-12-04 22:30:41 +0100

    chore: update kanidm

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -17,23 +17,6 @@
         '';
     });
 
-    kanidm = prev.kanidm.overrideAttrs (_finalAttrs: _previousAttrs: {
-      patches = [
-        (prev.fetchpatch {
-          name = "group-list-json-output.patch";
-          url = "https://patch-diff.githubusercontent.com/raw/kanidm/kanidm/pull/2016.patch";
-          hash = "sha256-gc75KBzhth4fZvuvRa3Rjg1J7DIGy25mzUPCf2aha80=";
-        })
-        (prev.fetchpatch {
-          name = "person-and-oauth-json-output.patch";
-          url = "https://patch-diff.githubusercontent.com/raw/kanidm/kanidm/pull/2017.patch";
-          hash = "sha256-fZgJ7dY2LHvBi64A/6o7kfArUAsLqjWRRpH2q1GL5ic=";
-        })
-      ];
-
-      doCheck = false;
-    });
-
     formats =
       prev.formats
       // {

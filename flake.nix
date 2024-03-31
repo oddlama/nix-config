c3dcc619afe6e53e719aadf9ca5f3eed7ca9b96f
oddlama <oddlama@oddlama.org>
2024-03-31 23:43:44 +0200

    feat(topology): add automatic network coloring

--- flake.nix
+++ flake.nix
@@ -223,12 +223,12 @@
               networks.home-lan = {
                 name = "Home LAN";
                 cidrv4 = "192.168.1.0/24";
-                color = "#78dba9";
+                #color = "#78dba9";
               };
               networks.home-fritzbox = {
                 name = "Home Fritzbox";
                 cidrv4 = "192.168.178.0/24";
-                color = "#f1cf8a";
+                #color = "#f1cf8a";
               };
 
               nodes.ward.interfaces.lan.network = "home-lan";

52ed29bc3076974aee390f1055c6711642229498
oddlama <oddlama@oddlama.org>
2024-04-01 16:21:57 +0200

    chore: remove superfluous nix settings (same as defaults)

--- modules/config/nix.nix
+++ modules/config/nix.nix
@@ -21,8 +21,6 @@
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
-      cores = 0;
-      max-jobs = "auto";
     };
     daemonCPUSchedPolicy = "batch";
     daemonIOSchedPriority = 5;

a69f4577f0ea3ad5d4bedd6a9c1667246a8194de
oddlama <oddlama@oddlama.org>
2024-05-08 13:27:35 +0200

    fix: invalid unicode symbol map in kitty config

--- users/myuser/graphical/kitty.nix
+++ users/myuser/graphical/kitty.nix
@@ -15,7 +15,7 @@
       italic_font = "${config.programs.kitty.font.name} Italic";
       bold_italic_font = "${config.programs.kitty.font.name} Bold Italic";
       # Add nerd font symbol map. Not sure why it is suddenly needed since 0.32.0 (https://github.com/kovidgoyal/kitty/issues/7081)
-      symbol_map = "U+23FB-U+23FE,U+2665,U+26A1,U+2B58,U+E000-U+E00A,U+E0A0-U+E0A2,U+E0A3,U+E0B0-U+E0B3,U+E0B4-U+E0C8,U+E0CA,U+E0CC-U+E0D4,U+E200-U+E2A9,U+E300-U+E3E3,U+E5FA-U+E6B1,U+E700-U+E7C5,U+EA60-U+EBEB,U+F000-U+F2E0,U+F300-U+F372,U+F400-U+F532,U+F500-U+FD46,U+F0001-F1AF0 Symbols Nerd Font Mono";
+      symbol_map = "U+23FB-U+23FE,U+2665,U+26A1,U+2B58,U+E000-U+E00A,U+E0A0-U+E0A2,U+E0A3,U+E0B0-U+E0B3,U+E0B4-U+E0C8,U+E0CA,U+E0CC-U+E0D4,U+E200-U+E2A9,U+E300-U+E3E3,U+E5FA-U+E6B1,U+E700-U+E7C5,U+EA60-U+EBEB,U+F000-U+F2E0,U+F300-U+F372,U+F400-U+F532,U+F500-U+FD46,U+F0001-U+F1AF0 Symbols Nerd Font Mono";
 
       # Do not wait for inherited child processes.
       close_on_child_death = "yes";

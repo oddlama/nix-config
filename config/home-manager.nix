4a4743329bdc0010dbec295818f0fa8d6410e990
oddlama <oddlama@oddlama.org>
2024-06-03 22:45:11 +0200

    fix: slow zsh startup because of double compinit

--- config/home-manager.nix
+++ config/home-manager.nix
@@ -24,6 +24,13 @@
 
   # Required even when using home-manager's zsh module since the /etc/profile load order
   # is partly controlled by this. See nix-community/home-manager#3681.
-  # TODO remove once we have nushell
-  programs.zsh.enable = true;
+  # FIXME: remove once we have nushell
+  programs.zsh = {
+    enable = true;
+    # Disable the completion in the global module because it would call compinit
+    # but the home manager config also calls compinit. This causes the cache to be invalidated
+    # because the fpath changes in-between, causing constant re-evaluation and thus startup
+    # times of 1-2 seconds. Disable the completion here and only keep the home-manager one to fix it.
+    enableCompletion = false;
+  };
 }

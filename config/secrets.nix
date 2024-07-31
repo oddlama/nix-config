95818b6fcf7f1c33ca2fea646cb97f5f41681b7e
oddlama <oddlama@oddlama.org>
2024-08-01 01:36:14 +0200

    fix: regenerate password hashes correctly

--- config/secrets.nix
+++ config/secrets.nix
@@ -54,7 +54,8 @@
     echo " -> Deriving argon2id hash from [32m"${lib.escapeShellArg dep.host}":[m[33m"${lib.escapeShellArg dep.name}"[m" >&2
     ${decrypt} ${lib.escapeShellArg dep.file} \
       | tr -d '\n' \
-      | ${pkgs.libargon2}/bin/argon2 "$(${pkgs.openssl}/bin/openssl rand -base64 16)" -id \
+      | ${pkgs.libargon2}/bin/argon2 "$(${pkgs.openssl}/bin/openssl rand -base64 16)" -id -e \
+      | tr -d '\n' \
       || die "Failure while generating argon2id hash"
   '';
 

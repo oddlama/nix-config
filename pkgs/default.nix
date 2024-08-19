2fece6e5cc7cd49ba2104ba1725bafb703ea85b4
oddlama <oddlama@oddlama.org>
2024-08-19 13:48:22 +0200

    chore: time to downgrade, firefox is broken

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,21 +5,6 @@ _inputs: [
     deploy = prev.callPackage ./deploy.nix {};
     git-fuzzy = prev.callPackage ./git-fuzzy {};
     stalwart-mail = builtins.trace "remove once 0.9.1 is stable" (prev.callPackage ./stal.nix {});
-    delta = builtins.trace "remove once #334814" (prev.delta.overrideAttrs (old: rec {
-      version = "0.17.0-unstable-2024-08-12";
-
-      src = prev.fetchFromGitHub {
-        owner = "dandavison";
-        repo = "delta";
-        rev = "a01141b72001f4c630d77cf5274267d7638851e4";
-        hash = "sha256-My51pQw5a2Y2VTu39MmnjGfmCavg8pFqOmOntUildS0=";
-      };
-
-      cargoDeps = old.cargoDeps.overrideAttrs (_c_old: {
-        inherit src;
-        outputHash = "sha256-TJ/yLt53hKElylycUfGV8JGt7GzqSnIO3ImhZvhVQu0=";
-      });
-    }));
     kanidm = prev.kanidm.overrideAttrs (old: let
       provisionSrc = prev.fetchFromGitHub {
         owner = "oddlama";

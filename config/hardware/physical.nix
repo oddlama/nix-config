88e6b6e22f5c1509123e62149bc3d676573b3a96
oddlama <oddlama@oddlama.org>
2024-11-29 14:05:01 +0100

    chore: disable fwupd service

--- config/hardware/physical.nix
+++ config/hardware/physical.nix
@@ -12,7 +12,6 @@
   };
 
   services = lib.mkIf (!minimal) {
-    fwupd.enable = true;
     smartd.enable = true;
     thermald.enable = builtins.elem config.nixpkgs.hostPlatform.system [ "x86_64-linux" ];
   };

eacb196046ee4d7399eea4fc7a98c569c7323eb5
oddlama <oddlama@oddlama.org>
2025-03-02 17:56:58 +0100

    chore: bye webcord

--- users/myuser/graphical/discord.nix
+++ users/myuser/graphical/discord.nix
@@ -2,11 +2,9 @@
 {
   home.packages = with pkgs; [
     discord
-    webcord
   ];
 
   home.persistence."/state".directories = [
     ".config/discord" # Bad Discord! BAD! Saves its state in .config tststs
-    ".config/WebCord" # You too, WebCord!
   ];
 }

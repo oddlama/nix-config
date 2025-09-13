cd896819f96f540523ab2fec06649ce46683db4c
oddlama <oddlama@oddlama.org>
2025-09-14 00:02:42 +0200

    chore: fix master identity path

--- flake/agenix-rekey.nix
+++ flake/agenix-rekey.nix
@@ -12,7 +12,7 @@
     # The identities that are used to rekey agenix secrets and to
     # decrypt all repository-wide secrets.
     secretsConfig = {
-      masterIdentities = [ "\"$DEVSHELL_DIR\"/secrets/yk1-nix-rage.pub" ];
+      masterIdentities = [ "\"$PRJ_ROOT\"/secrets/yk1-nix-rage.pub" ];
       extraEncryptionPubkeys = [ ../secrets/backup.pub ];
     };
   };

433eb2f4f8a7b3084d9412f3caf231f8e8ea6b75
oddlama <oddlama@oddlama.org>
2025-10-11 20:57:40 +0200

    chore: remove zrepl in favor of hetzner storage box via restic

--- hosts/sire/fs.nix
+++ hosts/sire/fs.nix
@@ -8,30 +8,29 @@ let
 in
 {
   disko.devices = {
-    disk =
-      {
-        m2-ssd-1 = {
-          type = "disk";
-          device = "/dev/disk/by-id/${disks.m2-ssd-1}";
-          content = {
-            type = "gpt";
-            partitions = {
-              efi = lib.disko.gpt.partEfi "1G";
-              rpool = lib.disko.gpt.partLuksZfs disks.m2-ssd-1 "rpool" "100%";
-            };
+    disk = {
+      m2-ssd-1 = {
+        type = "disk";
+        device = "/dev/disk/by-id/${disks.m2-ssd-1}";
+        content = {
+          type = "gpt";
+          partitions = {
+            efi = lib.disko.gpt.partEfi "1G";
+            rpool = lib.disko.gpt.partLuksZfs disks.m2-ssd-1 "rpool" "100%";
           };
         };
-        m2-ssd-2 = {
-          type = "disk";
-          device = "/dev/disk/by-id/${disks.m2-ssd-2}";
-          content = lib.disko.content.luksZfs disks.m2-ssd-2 "rpool";
-        };
-      }
-      // lib.genAttrs disks.hdds-storage (disk: {
+      };
+      m2-ssd-2 = {
         type = "disk";
-        device = "/dev/disk/by-id/${disk}";
-        content = lib.disko.content.luksZfs disk "storage";
-      });
+        device = "/dev/disk/by-id/${disks.m2-ssd-2}";
+        content = lib.disko.content.luksZfs disks.m2-ssd-2 "rpool";
+      };
+    }
+    // lib.genAttrs disks.hdds-storage (disk: {
+      type = "disk";
+      device = "/dev/disk/by-id/${disk}";
+      content = lib.disko.content.luksZfs disk "storage";
+    });
     zpool = {
       rpool = lib.disko.zfs.mkZpool {
         mode = "mirror";
@@ -49,71 +48,4 @@ in
   };
 
   boot.initrd.systemd.services."zfs-import-storage".after = [ "cryptsetup.target" ];
-
-  services.zrepl = {
-    enable = true;
-    settings = {
-      global = {
-        logging = [
-          {
-            type = "syslog";
-            level = "info";
-            format = "human";
-          }
-        ];
-        # TODO zrepl monitor
-        #monitoring = [
-        #  {
-        #    type = "prometheus";
-        #    listen = ":9811";
-        #    listen_freebind = true;
-        #  }
-        #];
-      };
-
-      jobs = [
-        {
-          name = "local-snapshots";
-          type = "snap";
-          filesystems = {
-            "rpool/local/state<" = true;
-            "rpool/safe<" = true;
-            "storage/safe<" = true;
-            "storage/bunker<" = true;
-          };
-          snapshotting = {
-            type = "periodic";
-            prefix = "zrepl-";
-            timestamp_format = "iso-8601";
-            interval = "15m";
-          };
-          pruning.keep = [
-            # Keep all manual snapshots
-            {
-              type = "regex";
-              regex = "^zrepl-.*$";
-              negate = true;
-            }
-            # Keep last n snapshots
-            {
-              type = "last_n";
-              regex = "^zrepl-.*$";
-              count = 10;
-            }
-            # Prune periodically
-            {
-              type = "grid";
-              regex = "^zrepl-.*$";
-              grid = lib.concatStringsSep " | " [
-                "72x1h"
-                "90x1d"
-                "60x1w"
-                "24x30d"
-              ];
-            }
-          ];
-        }
-      ];
-    };
-  };
 }

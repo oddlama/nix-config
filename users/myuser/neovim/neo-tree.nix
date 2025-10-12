a4769eb35dffdb81a5d0a0698353964bf18f96d2
oddlama <oddlama@oddlama.org>
2025-10-12 19:19:20 +0200

    chore: update neotree config

--- users/myuser/neovim/neo-tree.nix
+++ users/myuser/neovim/neo-tree.nix
@@ -1,62 +1,64 @@
 {
   programs.nixvim.plugins.neo-tree = {
     enable = true;
-    sortCaseInsensitive = true;
-    usePopupsForInput = false;
-    popupBorderStyle = "rounded";
-    # TODO window_opts.winblend = 0;
-    window = {
-      width = 34;
-      position = "left";
-      mappings = {
-        "<CR>" = "open_with_window_picker";
-        "s" = "split_with_window_picker";
-        "v" = "vsplit_with_window_picker";
-        "t" = "open_tabnew";
-        "z" = "close_all_nodes";
-        "Z" = "expand_all_nodes";
-        "a".__raw =
-          # lua
-          ''{ "add", config = { show_path = "relative" } }'';
-        "A".__raw =
-          # lua
-          ''{ "add_directory", config = { show_path = "relative" } }'';
-        "c".__raw =
-          # lua
-          ''{ "copy", config = { show_path = "relative" } }'';
-        "m".__raw =
-          # lua
-          ''{ "move", config = { show_path = "relative" } }'';
+    settings = {
+      sort_case_insensitive = true;
+      use_popups_for_input = false;
+      popup_border_style = "rounded";
+      # TODO window_opts.winblend = 0;
+      window = {
+        width = 34;
+        position = "left";
+        mappings = {
+          "<CR>" = "open_with_window_picker";
+          "s" = "split_with_window_picker";
+          "v" = "vsplit_with_window_picker";
+          "t" = "open_tabnew";
+          "z" = "close_all_nodes";
+          "Z" = "expand_all_nodes";
+          "a".__raw =
+            # lua
+            ''{ "add", config = { show_path = "relative" } }'';
+          "A".__raw =
+            # lua
+            ''{ "add_directory", config = { show_path = "relative" } }'';
+          "c".__raw =
+            # lua
+            ''{ "copy", config = { show_path = "relative" } }'';
+          "m".__raw =
+            # lua
+            ''{ "move", config = { show_path = "relative" } }'';
+        };
       };
-    };
-    defaultComponentConfigs = {
-      modified.symbol = "~ ";
-      indent.withExpanders = true;
-      name.trailingSlash = true;
-      gitStatus.symbols = {
-        added = "+";
-        deleted = "✖";
-        modified = "";
-        renamed = "➜";
-        untracked = "?";
-        ignored = "󰛑";
-        unstaged = ""; # 󰄱
-        staged = "󰄵";
-        conflict = "";
+      default_component_configs = {
+        modified.symbol = "~ ";
+        indent.with_expanders = true;
+        name.trailing_slash = true;
+        git_status.symbols = {
+          added = "+";
+          deleted = "✖";
+          modified = "";
+          renamed = "➜";
+          untracked = "?";
+          ignored = "󰛑";
+          unstaged = ""; # 󰄱
+          staged = "󰄵";
+          conflict = "";
+        };
       };
-    };
-    filesystem = {
-      window.mappings = {
-        "gA" = "git_add_all";
-        "ga" = "git_add_file";
-        "gu" = "git_unstage_file";
-      };
-      groupEmptyDirs = true;
-      followCurrentFile.enabled = true;
-      useLibuvFileWatcher = true;
-      filteredItems = {
-        hideDotfiles = false;
-        hideByName = [ ".git" ];
+      filesystem = {
+        window.mappings = {
+          "gA" = "git_add_all";
+          "ga" = "git_add_file";
+          "gu" = "git_unstage_file";
+        };
+        group_empty_dirs = true;
+        follow_current_file.enabled = true;
+        use_libuv_file_watcher = true;
+        filtered_items = {
+          hide_dotfiles = false;
+          hide_by_name = [ ".git" ];
+        };
       };
     };
   };

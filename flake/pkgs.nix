63d50cdc91b5f27d8776d4dfb0caa8babc678230
oddlama <oddlama@oddlama.org>
2026-02-28 16:06:25 +0100

    feat: add pkgsCuda

--- flake/pkgs.nix
+++ flake/pkgs.nix
@@ -2,17 +2,19 @@
 {
   imports = [
     (
-      {
-        lib,
-        flake-parts-lib,
-        ...
-      }:
+      { lib, flake-parts-lib, ... }:
       flake-parts-lib.mkTransposedPerSystemModule {
         name = "pkgs";
         file = ./pkgs.nix;
-        option = lib.mkOption {
-          type = lib.types.unspecified;
-        };
+        option = lib.mkOption { type = lib.types.unspecified; };
+      }
+    )
+    (
+      { lib, flake-parts-lib, ... }:
+      flake-parts-lib.mkTransposedPerSystemModule {
+        name = "pkgsCuda";
+        file = ./pkgs.nix;
+        option = lib.mkOption { type = lib.types.unspecified; };
       }
     )
   ];
@@ -36,5 +38,17 @@
       };
 
       inherit pkgs;
+
+      pkgsCuda = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        config.segger-jlink.acceptLicense = true;
+        config.cudaSupport = true;
+        overlays = (import ../pkgs/default.nix inputs) ++ [
+          inputs.nix-topology.overlays.default
+          # inputs.nixos-cosmic.overlays.default
+          inputs.nixos-extra-modules.overlays.default
+        ];
+      };
     };
 }

d4c21a01b82c0f3a5afcc345a4a95d3a763329f3
oddlama <oddlama@oddlama.org>
2026-03-26 14:02:52 +0100

    feat: fix diagnostic warning

--- users/malte/neovim/lsp.nix
+++ users/malte/neovim/lsp.nix
@@ -4,85 +4,84 @@
   ...
 }:
 {
-  programs.nixvim.plugins = {
-    jdtls = {
-      enable = true;
-      settings.cmd = [
-        "${lib.getExe pkgs.jdt-language-server}"
-        "-data"
-        "~/.cache/jdtls/workspace"
-      ];
-    };
-    lsp = {
-      enable = true;
-      preConfig =
-        # lua
-        ''
-          local lsp_symbol = function(name, icon)
-          vim.fn.sign_define(
-            "DiagnosticSign" .. name,
-            { text = icon, numhl = "Diagnostic" .. name, texthl = "Diagnostic" .. name }
-          )
-          end
+  programs.nixvim = {
+    plugins = {
+      jdtls = {
+        enable = true;
+        settings.cmd = [
+          "${lib.getExe pkgs.jdt-language-server}"
+          "-data"
+          "~/.cache/jdtls/workspace"
+        ];
+      };
+      lsp = {
+        enable = true;
+        servers = {
+          bashls.enable = true;
+          cssls.enable = true;
+          html.enable = true;
+          clangd.enable = true;
+          # rust-analyzer should not be set here because of rustaceanvim
+          nil_ls = {
+            enable = true;
+            settings = {
+              formatting.command = [
+                (lib.getExe pkgs.nixfmt)
+                "--quiet"
+              ];
+            };
+          };
+          nixd.enable = true;
+        };
 
-          lsp_symbol("Error", "󰅙")
-          lsp_symbol("Info", "")
-          lsp_symbol("Hint", "󰌵")
-          lsp_symbol("Warn", "")
-        '';
-      servers = {
-        bashls.enable = true;
-        cssls.enable = true;
-        html.enable = true;
-        clangd.enable = true;
-        # rust-analyzer should not be set here because of rustaceanvim
-        nil_ls = {
-          enable = true;
-          settings = {
-            formatting.command = [
-              (lib.getExe pkgs.nixfmt-rfc-style)
-              "--quiet"
-            ];
+        #keymaps = {
+        #  diagnostic = {
+        #    "<leader>k" = "goto_prev";
+        #    "<leader>j" = "goto_next";
+        #  };
+        #  lspBuf = {
+        #    "gd" = "definition";
+        #    "gD" = "references";
+        #    "<leader>lt" = "type_definition";
+        #    "gi" = "implementation";
+        #    "K" = "hover";
+        #    "<leader>k" = "hover";
+        #    "<leader>r" = "rename";
+        #  };
+        #};
+      };
+
+      none-ls = {
+        enable = true;
+        sources = {
+          diagnostics = {
+            deadnix.enable = true;
+            gitlint.enable = true;
+            protolint.enable = true;
+          };
+          formatting = {
+            markdownlint.enable = true;
+            sqlfluff.enable = true;
+            shfmt.enable = true;
           };
         };
-        nixd.enable = true;
       };
 
-      #keymaps = {
-      #  diagnostic = {
-      #    "<leader>k" = "goto_prev";
-      #    "<leader>j" = "goto_next";
-      #  };
-      #  lspBuf = {
-      #    "gd" = "definition";
-      #    "gD" = "references";
-      #    "<leader>lt" = "type_definition";
-      #    "gi" = "implementation";
-      #    "K" = "hover";
-      #    "<leader>k" = "hover";
-      #    "<leader>r" = "rename";
-      #  };
-      #};
+      dap.enable = true;
+      dap-virtual-text.enable = true;
+      dap-ui.enable = true;
     };
 
-    none-ls = {
-      enable = true;
-      sources = {
-        diagnostics = {
-          deadnix.enable = true;
-          gitlint.enable = true;
-          protolint.enable = true;
-        };
-        formatting = {
-          markdownlint.enable = true;
-          sqlfluff.enable = true;
-          shfmt.enable = true;
-        };
-      };
+    diagnostic.settings = {
+      virtual_text = true;
+      signs.text.__raw = ''
+        {
+          [vim.diagnostic.severity.ERROR] = "󰅙",
+          [vim.diagnostic.severity.WARN] = "",
+          [vim.diagnostic.severity.HINT] = "󰌵",
+          [vim.diagnostic.severity.INFO] = "",
+        }
+      '';
     };
-
-    dap.enable = true;
-    dap-virtual-text.enable = true;
-    dap-ui.enable = true;
   };
 }

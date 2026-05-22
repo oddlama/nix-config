335ffc9d3a1408024c844353d87dd7cfd69ab071
oddlama <oddlama@oddlama.org>
2026-05-22 22:15:14 +0200

    feat: add deepfilternet for mic denoising

--- config/optional/sound.nix
+++ config/optional/sound.nix
@@ -30,6 +30,52 @@ lib.optionalAttrs (!minimal) {
       176400
       192000
     ];
+
+    extraLadspaPackages = [ pkgs.deepfilternet ];
+    extraConfig.pipewire = {
+      "92-quantum"."context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 512;
+        "default.clock.min-quantum" = 512;
+        "default.clock.max-quantum" = 512;
+      };
+      "99-input-denoising" = {
+        "context.properties" = {
+          "link.max-buffers" = 16;
+          "core.daemon" = true;
+          "core.name" = "pipewire-0";
+          "module.x11.bell" = false;
+          "module.access" = true;
+          "module.jackdbus-detect" = false;
+        };
+
+        "context.modules" = [
+          {
+            name = "libpipewire-module-filter-chain";
+            args = {
+              "node.description" = "DeepFilter Noise Canceling source";
+              "media.name" = "DeepFilter Noise Canceling source";
+
+              "filter.graph".nodes = [
+                {
+                  type = "ladspa";
+                  name = "DeepFilter Mono";
+                  plugin = "libdeep_filter_ladspa";
+                  label = "deep_filter_mono";
+                  control."Attenuation Limit (dB)" = 100;
+                }
+              ];
+
+              "audio.rate" = 48000;
+              "audio.position" = "[MONO]";
+
+              "capture.props"."node.passive" = true;
+              "playback.props"."media.class" = "Audio/Source";
+            };
+          }
+        ];
+      };
+    };
   };
 
   environment.systemPackages = with pkgs; [
